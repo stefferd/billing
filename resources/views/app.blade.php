2a47bddd319787ab902f922527c94e64aa4f8f56
Stef van den Berg <stef@debmedia.nl>
2015-07-22 10:34:04 +0200

    Updated the package json

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ URL::asset('assets/lib/angular-material/angular-material.css') }}" />
 	<title>Perscriptio</title>
-    <base href="/" />
 </head>
 <body>
     <div ui-view></div>
